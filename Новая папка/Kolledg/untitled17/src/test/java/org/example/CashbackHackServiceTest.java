2756468af29dcf9aa795a05d4e9f80dcd13a3919
Kostya <tsarevkostya03@gmail.com>
2026-06-05 18:24:01 +0300

    Add TestNG tests

--- Новая папка/Kolledg/untitled17/src/test/java/org/example/CashbackHackServiceTest.java
+++ Новая папка/Kolledg/untitled17/src/test/java/org/example/CashbackHackServiceTest.java
@@ -1,29 +1,28 @@
 package org.example;
 
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
+
     @Test
     public void shouldReturn100WhenAmount900() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(900);
-        assertEquals(result, 100, "Для 900 рублей нужно докупить 100");
+        assertEquals(result, 100, "For 900 rubles need 100");
     }
 
     @Test
     public void shouldReturn500WhenAmount1500() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(1500);
-        assertEquals(result, 500, "Для 1500 рублей нужно докупить 500");
+        assertEquals(result, 500, "For 1500 rubles need 500");
     }
 
     @Test
     public void shouldReturn0WhenAmount1000() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(1000);
-        assertEquals(result, 0, "Для 1000 рублей докупать не нужно");
+        assertEquals(result, 0, "For 1000 rubles need 0");
     }
-    // Этот тест упадёт, потому что реальный результат = 1000
 }
